7ec3931ff9a314787485e4702f82618fe897e887
parsentev <parsentev@yandex.ru>
2013-06-04 16:54:31 +0400

    тс 5 не работает с CSV import [#80397]

--- src/com/trackstudio/csvimport/QBConverter.java
+++ src/com/trackstudio/csvimport/QBConverter.java
@@ -3,34 +3,48 @@ package com.trackstudio.csvimport;
 import au.com.bytecode.opencsv.CSVReader;
 import com.trackstudio.data.DataBean;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class QBConverter {
     private DataBean dataBean;
     private String[] header;
-    private List<String[]> lines = new ArrayList<String[]>();
+    private List<String[]> lines;
+    private Map<String, String> keyHeader;
 
     public QBConverter(DataBean dataBean) {
         this.dataBean = dataBean;
     }
 
     public void updateData() throws IOException {
+        readKeyHeader();
         Reader readerFile = new InputStreamReader(new FileInputStream(this.dataBean.getFilePath()), this.dataBean.getEncoding());
         CSVReader reader = new CSVReader(readerFile, this.dataBean.getDelimiter().charAt(0));
         this.header = reader.readNext();
         String [] nextLine;
+        this.lines = new ArrayList<String[]>();
         while ((nextLine = reader.readNext()) != null) {
             this.lines.add(nextLine);
         }
     }
 
+    private void readKeyHeader() throws IOException {
+        keyHeader = new LinkedHashMap<String, String>();
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(dataBean.getMappingFile()));
+        for (Enumeration enums = prop.propertyNames();enums.hasMoreElements();){
+            String name = enums.nextElement().toString();
+            keyHeader.put(prop.getProperty(name), name);
+        }
+    }
+
     public String[] getHeaders() {
-        return header;
+        String[] names = new String[header.length];
+        int inx = 0;
+        for (String name : header) {
+            names[inx++]=keyHeader.get(name);
+        }
+        return names;
     }
 
     public List<String[]> getLines() {
